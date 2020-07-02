4db541eb3ea13e62c6bee5329067a4d1399aa5f5
tharindu97 <tharindukavishna@gmail.com>
2020-07-02 10:03:52 +0530

    comleted

--- Basic_test/app.js
+++ Basic_test/app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+let isAngry = true;
+const element = 'img';
+const angryEmoji = 'angry.png';
+const hahaEmoji = 'haha.jpeg';
+
+function Click(){
+
+    isAngry = !isAngry;
+    if(isAngry){
+        document.getElementById(element).src = hahaEmoji;
+    }else{
+        document.getElementById(element).src = angryEmoji;
+    }
+}
